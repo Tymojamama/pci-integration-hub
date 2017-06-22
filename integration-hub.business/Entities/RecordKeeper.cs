126247e44830f7a7a3ea0b94c4da6f6cb52d6e99
Zach Allen <zallen@pension-consultants.com>
2017-06-22 11:44:26 -0500

    new entities

---
+++ integration-hub.business/Entities/RecordKeeper.cs
@@ -0,0 +1,87 @@
+﻿using PensionConsultants.Data.Access;
+using PensionConsultants.Data.Utilities;
+using DataIntegrationHub.Business.Components;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataIntegrationHub.Business.Entities
+{
+    public class RecordKeeper : IDatabaseEntity
+    {
+        #region PublicMembers
+
+        public Guid RecordKeeperId { get; private set; }
+        public Guid ModifiedBy { get; private set; }
+        public Guid CreatedBy { get; private set; }
+
+        public string Name { get; private set; }
+        public string RfpContactName { get; private set; }
+        public string RfpContactEmail { get; private set; }
+
+        public DateTime ModifiedOn { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+
+        public int StateCode { get; private set; }
+
+        #endregion
+
+        #region PrivateMembers
+
+        private bool ExistingRecord;
+
+        #endregion
+
+        public RecordKeeper(Guid _recordKeeperId)
+        {
+            this.RecordKeeperId = _recordKeeperId;
+
+            ExistingRecord = true;
+
+            DataTable _dataTable = GetDetailsFromId();
+            DataRow _dataRow = _dataTable.Rows[0];
+
+            Guid _modifiedBy;
+            Guid _createdBy;
+            DateTime _modifiedOn;
+            DateTime _createdOn;
+
+            int _stateCode;
+
+            this.Name = _dataRow["Name"].ToString();
+            this.RfpContactName = _dataRow["PrimaryRfpContactName"].ToString();
+            this.RfpContactEmail = _dataRow["PrimaryRfpContactEmail"].ToString();
+
+            StringParser.Parse(_dataRow["ModifiedBy"].ToString(), out _modifiedBy);
+            StringParser.Parse(_dataRow["CreatedBy"].ToString(), out _createdBy);
+            StringParser.Parse(_dataRow["ModifiedOn"].ToString(), out _modifiedOn);
+            StringParser.Parse(_dataRow["CreatedOn"].ToString(), out _createdOn);
+            StringParser.Parse(_dataRow["StateCode"].ToString(), out _stateCode);
+
+            this.ModifiedBy = _modifiedBy;
+            this.ModifiedOn = _modifiedOn;
+            this.CreatedBy = _createdBy;
+            this.CreatedOn = _createdOn;
+            this.StateCode = _stateCode;
+        }
+
+        private DataTable GetDetailsFromId()
+        {
+            // Prepare parameter list for stored procedure
+            Hashtable parameterList = new Hashtable();
+            parameterList.Add("@RecordKeeperId", this.RecordKeeperId);
+
+            // Execute stored procedure and return datatable result
+            return Database.DataIntegrationHub.ExecuteStoredProcedureQuery("[dbo].[usp_RecordKeeperGetDetailsFromId]", parameterList);
+        }
+
+        public static DataTable GetAll()
+        {
+            return Database.DataIntegrationHub.ExecuteStoredProcedureQuery("[dbo].[usp_RecordKeeperGetAll]");
+        }
+    }
+}
